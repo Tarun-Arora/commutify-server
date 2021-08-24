4f2ca2ece4fec29d4784d617ea9fa526557b44fa
Devashish Rai <devashish.rai.cse20@itbhu.ac.in>
2021-08-25 00:06:04 +0530

    Setup for websocket

--- commutify/authentication/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import UserInfo, ForgotPwdRequest
-admin.site.register(UserInfo)
-admin.site.register(ForgotPwdRequest)
