1ffc28e22103acb2e47e0f0da5dcc65aac67e144
Tarun <tarun.arora.cse20@iitbhu.ac.in>
2021-11-13 00:15:14 +0530

    Deployment

--- commutify/settings.py
+++ commutify/settings.py
@@ -82,12 +82,8 @@ WSGI_APPLICATION = 'commutify.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': config('DB_NAME'),
-        'HOST': config('DB_HOST'),
-        'PORT': '3306',
-        'USER': config('DB_USER'),
-        'PASSWORD': config('DB_PASSWORD'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
